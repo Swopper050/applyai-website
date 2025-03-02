ea384a0c787949e6744befa0522c50405485f2cd
Swopper050 <bram@witoos.nl>
2025-03-02 18:11:48 +0100

    Updated email templates

--- api/app/mail_utils.py
+++ api/app/mail_utils.py
@@ -1,9 +1,31 @@
 from datetime import datetime
 from string import Template
+import os
 
 from flask_mail import Message
 
 from app.extensions import mail
+
+
+def _attach_email_images(message):
+    """Attach logo and signature images to the email message as inline images."""
+    with open("./email_templates/ApplyAI logo.png", "rb") as logo_file:
+        message.attach(
+            "ApplyAI logo.png",
+            "image/png",
+            logo_file.read(),
+            "inline",
+            headers={"Content-ID": "<logo>"},
+        )
+
+    with open("./email_templates/logo_signature.png", "rb") as signature_file:
+        message.attach(
+            "logo_signature.png",
+            "image/png",
+            signature_file.read(),
+            "inline",
+            headers={"Content-ID": "<signature>"},
+        )
 
 
 def send_contact_confirmation_email(
@@ -18,10 +40,12 @@ def send_contact_confirmation_email(
     )
 
     email_message = Message(
-        subject="🛁 ApplyAI - Thank you for your message",
+        subject="ApplyAI - Bedankt voor je bericht",
         recipients=[email],
         html=html_content,
     )
+
+    _attach_email_images(email_message)
 
     mail.send(email_message)
 
@@ -40,9 +64,33 @@ def send_contact_notification_email(
     )
 
     email_message = Message(
-        subject="🛁 ApplyAI - New Contact Form Submission",
+        subject="ApplyAI - Nieuwe inzending contactformulier",
         recipients=["info@applyai.nl"],
         html=html_content,
         reply_to=email,
     )
+
+    _attach_email_images(email_message)
+
     mail.send(email_message)
+
+
+def send_verification_email(*, email: str, verification_link: str):
+    """Send an email verification link to the user."""
+    with open("./email_templates/verify_email.html", "r") as file:
+        html_content = file.read()
+
+    html_content = Template(html_content).safe_substitute(
+        verification_link=verification_link
+    )
+
+    email_message = Message(
+        subject="ApplyAI - Verifieer je e-mailadres",
+        recipients=[email],
+        html=html_content,
+    )
+
+    _attach_email_images(email_message)
+
+    mail.send(email_message)
+
